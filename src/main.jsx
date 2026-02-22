484f539bf09090a9cc7ce62ce9a18c3560be13ed
SadikIhtesham <146316781+SadikIhtesham@users.noreply.github.com>
2026-02-23 01:39:24 +0600

    Initial trying

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import Currency_converter from './Currency_converter'
+
+createRoot(document.getElementById('root')).render(
+  <>
+    <Currency_converter />
+  </>
+)
